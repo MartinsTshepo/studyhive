4761485e02e0ca057a676fb5d6681b8bb8322492
MartinsTshepo <tshepokingmartins1@gmail.com>
2026-06-01 16:39:47 +0200

    Refactor Supabase client initialization

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPA_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPA_KEY = import.meta.env.VITE_SUPABASE_KEY
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
 
-export const supabase = createClient(SUPA_URL, SUPA_KEY)
+export default supabase
